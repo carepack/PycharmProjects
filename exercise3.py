514b35dc0d1e231bd3888488a60ff2c87cd13207
michaeleisenhardt <em_prv@gmx.de>
2019-11-11 22:40:13 +0100

    Lists

---
+++ exercise3.py
@@ -0,0 +1,11 @@
+#list = [1, 2, 3, 4]
+#for element in list:
+#    print(element)
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for element in a:
+    if element < 5:
+        b = []
+        b.append(element)
+
+print(b)
