f2e91f647ef6e2810e2d27b1e4dbe449ca5c6ed8
me <meit130183@gmail.com>
2019-11-11 16:59:40 +0100

    Input Stream

---
+++ exercise1.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+name=input("Wie ist dein Name: ")
+age=input("Wie alt bist du: ")
+print("du heißt: " +name)
+print(name + ", du bist " + age + " alt")
+
+curr_year = date.today().year
+est_year = 100 - int(age) + curr_year
+est_year = str(est_year)
+line_out="Im Jahr: " + est_year + " wirst du 100 Jahre alt sein!\n"
+print(line_out)
+line_count=input("Wie oft soll die Ausgabe wiederholt werden? ")
+line_count=int(line_count)
+
+for i in range(line_count):
+    print(line_out)
+
+
+print(line_out * 5)
