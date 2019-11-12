a1f29a84edd7dd495deeec03d04fb0451de6ddbb
michaeleisenhardt <em_prv@gmx.de>
2019-11-12 04:08:28 +0100

    Lists

--- exercise3.py
+++ exercise3.py
@@ -1,11 +1,19 @@
-#list = [1, 2, 3, 4]
-#for element in list:
-#    print(element)
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b=[]
+c=[]
 
 for element in a:
     if element < 5:
-        b = []
+        print(element)
         b.append(element)
 
 print(b)
+
+in_num = int(input("Nenne eine Zahl: "))
+
+for element in a:
+    if element < in_num:
+        print(element)
+        c.append(element)
+
+print(c)
