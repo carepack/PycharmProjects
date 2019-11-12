895ece8a583573d04ae7f94b43dbb8d312332c81
me <meit130183@gmail.com>
2019-11-12 17:50:29 +0100

    comprehension ugly version

---
+++ exercise7.py
@@ -0,0 +1,15 @@
+import random
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+b = [num for num in a if num % 2 == 0]
+c = []
+print(b)
+
+
+
+for i in range(20):
+    c.append(random.randrange(100))
+
+d = [num for num in c if num % 2 == 0]
+print (c)
+print(d)
