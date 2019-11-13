157f91e7643aebf9ea2dc55d2b6174e906977725
me <meit130183@gmail.com>
2019-11-13 16:14:38 +0100

    overlapping list comprehension with random generated lists

---
+++ exercise10.py
@@ -0,0 +1,18 @@
+import random
+
+ran_num1 = random.randint(5,20)
+ran_num2 = random.randint(5,20)
+
+# random.sample generates no duplicates
+a = random.sample(range(100), ran_num1)
+b = random.sample(range(100), ran_num2)
+
+c = [num for num in a if num in b]
+
+print()
+print(a)
+print(b)
+print()
+print(c)
+
+
