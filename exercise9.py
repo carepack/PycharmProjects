a650996cc38c8dbc67b2bdf35f1ab7f9dd02f52c
me <meit130183@gmail.com>
2019-11-13 14:15:00 +0100

    modified version with optimizations according to the solution

--- exercise9.py
+++ exercise9.py
@@ -1,9 +1,11 @@
 import random
+count = 0
+num = random.randint(1, 9)
+inp_user = 0
 
-while str != "Exit":
-    num = random.randint(1,9)
-    print(num)
-    inp_user = int(input("Bitte eine Zahl eingeben: "))
+while inp_user != "Exit" and inp_user != num:
+    count = count + 1
+    inp_user = int(input("Bitte eine Zahl oder Exit eingeben: "))
     if inp_user < num:
         print("\nLeider nein, ihre Zahl liegt unterhalb der gesuchten: %s" % num)
     elif inp_user > num:
@@ -11,6 +13,6 @@ while str != "Exit":
     else:
         print("\nHerzlichen Glückwunsch! ihre Zahl %s stimmt mit der gesuchten Zahl %s überein" % (inp_user, num))
         print("Herzlichen Glückwunsch! ihre Zahl {0} stimmt mit der gesuchten Zahl {1} überein".format(inp_user, num))
-    str = input("\nZum beenden bitte Exit eingeben:\n ")
+        print("Sie haben {0} Versuche gebraucht, um die richtige Zahl zu finden".format(count))
 
 
