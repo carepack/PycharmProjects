b4da0f0b90539e6ebc0b4464aa0c9e12804509e0
me <meit130183@gmail.com>
2019-11-14 11:29:21 +0100

    check input for prime numbers

--- exercise11.py
+++ exercise11.py
@@ -1,11 +1,17 @@
 # 13.11.2019
 # Auf Primzahl prüfen mit verwenden einer Funktion
+# while / else wird ausgeführt wenn while Bedingung false bekommt / Zähler abgelaufen ist
 
 def find_prime(user_value):
+    i = 2
+    while i < (user_value):
+        prime = user_value % i
+        if prime != 0:
+            i += 1
+        else:
+            return ("ist keine Primzahl da durch %s teilbar" % i)
+    else:
+        return ("ist eine Primzahl")
 
-
-
-
-
-user_in = input("Bite eine Zahl eingeben: ")
-print(find_prime(user_in))
+user_in = int(input("Bite eine Zahl eingeben: "))
+print("Ihre Zahl " + str(find_prime(user_in)))
