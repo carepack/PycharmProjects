dae5a0d447575022a7b5cf5413299e265ab664fe
me <meit130183@gmail.com>
2019-11-15 10:20:53 +0100

    fibonacci list of length from user input

---
+++ exercise13.py
@@ -0,0 +1,17 @@
+# 15.11.2019
+# generate fibonacci numbers with function
+
+def gen_fibo(user_val):
+    i = 0
+    j = 1
+    count = 0
+    a = [0, 1]
+    while count < (user_val - 2):
+        a.append(a[i] + a[j])
+        i += 1
+        j += 1
+        count += 1
+    return a
+
+user_in = int(input("Bitte die Anzahl der Fibonacci Zahlen zur Ausgabe eingeben: "))
+print(str(gen_fibo(user_in)))
