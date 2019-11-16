9db0fad8a7b6897e8dca719817ba873b007bc9c6
michaeleisenhardt <em_prv@gmx.de>
2019-11-16 09:53:57 +0100

    simple pw generator

--- exercise16.py
+++ exercise16.py
@@ -1,6 +1,31 @@
-#import os.urandom()
-import os, random, string
-c = os.urandom(10)
-b = SystemRandom(hrlp)
-print(c)
-print(b)
+#!/opt/local/bin/python3.7
+# 16.11.2019
+# simple pw generator - main func, join, secrets ad check python version
+# executable from commandline
+
+import sys
+import string
+import secrets
+
+def choice():
+    user_in = input("Choose your password complexity and enter the length: ")
+    return user_in
+
+def gen_pw(answ):
+    signs = str.strip(string.printable)
+    password = ''.join(secrets.choice(signs) for i in range(int(answ)))
+    print(password)
+    once_again = input("Generate another one: yes / no? ")
+    if once_again == "yes":
+        main()
+    else:
+        return
+
+def main():
+    answ = choice()
+    gen_pw(answ)
+
+if __name__ == '__main__':
+    if sys.version_info[0] < 3:
+        raise Exception("Python 3 or a more recent version is required.")
+    main()
