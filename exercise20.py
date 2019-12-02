5ab48ff87c3e2a9f527e24dc90793ffab018ffb0
me <meit130183@gmail.com>
2019-12-02 16:51:40 +0100

    initial commit

---
+++ exercise20.py
@@ -0,0 +1,15 @@
+#
+#
+
+import sys
+
+
+
+def main():
+
+
+
+if __name__ = '__main__':
+    if sys.version_info[0] < 3:
+        raise Exception("Python 3 or a more recent version is required.")
+    main()
