1a09d4e782fa3b5fbb46c1e7be36bba5a6d24c2b
me <meit130183@gmail.com>
2019-12-03 17:02:34 +0100

    only for syncing between devices

--- exercise20.py
+++ exercise20.py
@@ -1,15 +1,42 @@
-#
-#
+# 03.12.2019
+# Binary search in a list if the guess is in
 
 import sys
 
 
 
 def main():
+    run = True
+    ordered_List = [9, 16, 33, 67, 118, 119, 143, 149, 160]
+    guess = int(input("Please give number"))
+    len_list = round(len(ordered_List) / 2)
+    print(str(ordered_List[len_list]))
+    #print(len_list)
+    while True:
+
+        if len_list == 0 or len_list >= (len(ordered_List) - 1):
+            print("Number not included in list")
+            return False
+
+        if guess == ordered_List[len_list]:
+            print ("Thats your Number: " + str(ordered_List[len_list]))
+            return False
+        elif guess < ordered_List[len_list]:
+            len_list -= 1
+        elif guess > ordered_List[len_list]:
+            len_list += 1
 
 
 
-if __name__ = '__main__':
+    #else:
+    #    print("Number not included in list!")
+
+
+
+
+
+
+if __name__ == '__main__':
     if sys.version_info[0] < 3:
         raise Exception("Python 3 or a more recent version is required.")
     main()
