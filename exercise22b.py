03ae43604fcf56c89aefa1e13661bc79f45da98d
me <meit130183@gmail.com>
2019-12-06 13:35:31 +0100

    reading from file and manipulate content - advanced

--- exercise22b.py
+++ exercise22b.py
@@ -1,39 +1,20 @@
 # 05.12.2019
 # Read from file an work with the strings
-# /a/anechoic_chamber/sun_agrlvogbxdobprem.jpg
+
 import sys
-from array import *
-
-
-
-
 
 def main():
-    file_list = []
-    column = []
-    tmp  =[]
-    dict = {}
+    counter_dict = {}
     with open("Training_01.txt", "r") as source_file:
-        for lines in source_file:
-            file_list.append(lines.replace("\n", ""))
-            tmp = lines.split("/", 3)
-            dict['1'] = tmp[1]
-            dict['2'] = tmp[2]
-            print(dict)
-            #column[0].append(tmp[1])
-            #column[0][0].append(tmp[2])
-            #column[0][0][0].append(tmp[3])
-        print(tmp)
-
-
-        #for el in file_list:
-            #el.strip()
-        #    print(el)
-
-
-
-
-
+        line = source_file.readline()
+        while line:
+            line = line[3:-26]
+            if line in counter_dict:
+                counter_dict[line] +=1
+            else:
+                counter_dict[line] = 1
+            line = source_file.readline()
+    print(counter_dict)
 
 if __name__ == '__main__':
     if sys.version_info[0] < 3:
