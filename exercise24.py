d64984d3cacb2d6adf96fd091aad4951d7c549f7
me <meit130183@gmail.com>
2019-12-06 15:17:27 +0100

    initial commit

---
+++ exercise24.py
@@ -0,0 +1,15 @@
+# 06.12.2019
+# fake graphics printing with functions
+
+import sys
+
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    if sys.version_info[0] < 3:
+        raise Exception("Minimum requierment: Python3")
+    main()
