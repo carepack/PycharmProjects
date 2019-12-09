6264c1232da569a096752d6105e2a157cefd60ad
me <meit130183@gmail.com>
2019-12-09 15:18:32 +0100

    draw game board with functions

--- exercise24.py
+++ exercise24.py
@@ -1,15 +1,45 @@
 # 06.12.2019
-# fake graphics printing with functions
+# game board fake graphics printing with functions
 
 import sys
 
+def draw_dot(x):
+    d = 0
+    while d < x:
+        if d < (x - 1):
+            print(" ...", end = "")
+            d += 1
+        else:
+            print(" ...")
+            d += 1
 
+def draw_lines(x):
+    d = 0
+    while d < x:
+        if d < (x - 1):
+            print("|   ", end = "")
+            d += 1
+        else:
+            print("|   ")
+            d += 1
 
 def main():
+    board_size = int(input("Please enter the gameboard size: "))
+    x = board_size
+    y = board_size + 1
+    i = 0
 
-
+    #while i < y:
+    for index in range(y):
+        if i < x:
+            draw_dot(x)
+            draw_lines(y)
+            i += 1
+        else:
+            draw_dot(x)
+            i += 1
 
 if __name__ == '__main__':
     if sys.version_info[0] < 3:
-        raise Exception("Minimum requierment: Python3")
+        raise Exception("Minimum requirement: Python3")
     main()
