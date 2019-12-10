8b6e8e315f4e6394a0e340a4eae4df193d273892
me <meit130183@gmail.com>
2019-12-10 16:25:35 +0100

    initial commit

---
+++ exercise26.py
@@ -0,0 +1,45 @@
+# 10.12.2019
+# check out the winner of tic tac toe
+
+import sys
+
+
+
+
+
+
+
+
+def main():
+	game = [[1, 2, 0],
+	        [2, 1, 0],
+	        [2, 1, 1]]
+	x = 0
+	y = 0
+	run = True
+
+	while run:
+		for el in game:
+
+	#print(game[0])
+	#print(len(game))
+
+	#while x < len(game):
+	#for el0, el1, el2 in zip(game):
+	#	if el0 and el1 and el2 == 1:
+			#print("blub")
+		#if game[0][x]
+
+	#check = ''.join(str(e) for e in game[0][0])
+	#print(check)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	if sys.version_info[0] < 3:
+		raise Exception("Minimum requirement: Python 3")
+	main()
